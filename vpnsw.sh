6284d9dde12bfd6d58ea9b36eff7078ad3ab0592
heyitsant <78290151+highdeftant@users.noreply.github.com>
2021-02-01 02:08:19 -0500

    Add files via upload
    This currently DOES NOT WORK, when switching locations it will revert you back to your original location, or just set you to washington-dc.

---
+++ vpnsw.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# VPN Switcher
+
+# Switches Wash-DC/UK-London
+
+LONDON=$(piactl set region uk-london)
+WASHDC=$(piactl set region us-washington-dc)
+REGION=$(piactl get region)
+
+
+echo $REGION
+
+function VPNLONDON()
+{
+	if [[ $REGION = "us-washington-dc" ]]
+	then
+		echo $LONDON
+		echo "Changing Connection to 'UK-London'."
+		sleep 9 	
+	elif [[ $REGION = "uk-london" ]]
+	then
+		echo $WASHDC
+		echo "Changing Connection to 'Wash-DC'."
+		sleep 9
+	fi
+}
+
+exit
+
