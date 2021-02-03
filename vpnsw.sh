5c787aad5cfc736ba9324aa2b641c48f489dcbe5
gh0st <gh0st@gh0st-AS2416@linklocal.com>
2021-02-03 16:07:15 -0500

    Update to VPN Switch that still doesnt work anyway

--- vpnsw.sh
+++ vpnsw.sh
@@ -8,21 +8,28 @@ LONDON=$(piactl set region uk-london)
 WASHDC=$(piactl set region us-washington-dc)
 REGION=$(piactl get region)
 
+function VPNLONDON() {
+	piactl set region uk-london
+	echo "Changing Connection to 'UK-London'."
+	sleep 3
+}
+
+VPNDC() {
+	piactl set region us-washington-dc
+	echo "Changing Connection to 'Wash-DC'."
+	sleep 3
+}
+
 
 echo $REGION
 
-if [[ $REGION = "uk-london" ]]
+if [[ $REGION == "us-washington-dc" ]]
 then
-	echo $WASHDC
-	echo "Changing Connection to 'Wash-DC'."
-	sleep 9
+	VPNLONDON
+	sleep 1
 
-elif [[ $REGION = "uk-london" ]]
+elif [[ $REGION == "uk-london" ]]
 then
-	echo $LONDON
-	echo "Changing Connection to 'UK-London'."
-	sleep 9
+	VPNDC
+	sleep 1
 fi
-
-exit
-
